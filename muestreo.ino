a486ac4b4c6eb01ea7cc61fd74cc9ebeb897e72f
Matias Adorno Mazzocato <102630642+matiasam459@users.noreply.github.com>
2023-12-04 15:35:31 -0400

    Update muestreo.ino

--- muestreo.ino
+++ muestreo.ino
@@ -1,26 +1,40 @@
-int Lm35=A0;
+int Lm35 = A0;
 float Voltaje, Valor, T;
-const int pinPWM = 9; 
+const int pinPWM = 9;
+int vueltas = 0;  // Variable para contar las vueltas
+float x = 0.0;    // Variable para el valor de x
 
-     
 void setup() {
-  pinMode(Lm35,INPUT);
-  pinMode(pinPWM, OUTPUT); 
+  pinMode(Lm35, INPUT);
+  pinMode(pinPWM, OUTPUT);
   Serial.begin(9600);
+  
 }
 
 void loop() {
   Valor = analogRead(Lm35);     // Lectura del sensor
-  Voltaje = Valor*(5./1023.);   // Calculo de Voltaje
-  T = Voltaje*100.;             // Cálculo de Temperatura
+  Voltaje = Valor * (5. / 1023.);   // Calculo de Voltaje
+  T = Voltaje * 100.;             // Cálculo de Temperatura
   
-  Serial.print("Voltaje: ");
-  Serial.print(Voltaje);      // Imprime el valor del voltaje y salta a la sig linea
+  Serial.print("Tiempo: ");
+  Serial.print(millis());
   Serial.print("\tTemperatura: ");
   Serial.print(T);
-  Serial.print("\tTiempo: ");
-  Serial.println(millis());  // Envía el tiempo en milisegundos
+  Serial.print("\tValor de entrada: ");
+  Serial.println(x);
 
-  analogWrite(pinPWM,102);
-  delay(250);
+  // Condiciones para cambiar el valor de x
+  if (vueltas < 20) {
+    x = 0.0;
+  } else if (vueltas < 500) {
+    x = 0.4;
+  } else {
+    x = 0.2;
+  }
+
+  analogWrite(pinPWM, 255 * x);
+
+  vueltas++;
+
+  delay(250);
 }
